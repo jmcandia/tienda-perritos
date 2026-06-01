759de4a0752e571053501bfc17d2c36cf0944457
José Miguel Candia <jmcandia@gmail.com>
2026-05-31 23:19:25 -0400

    Actualización del archivo server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const {
-  DB_HOST = "db",
+  DB_HOST = "10.0.2.203",
   DB_USER = "root",
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
